2a0f9d044be3e9e6d561af82d3c41e0b62c1c79b
txstu09 <sgillies09@gmail.com>
2017-11-27 00:50:44 -0600

    update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+#Twitter API keys for @txstu09
+
 consumer_key = "avKFx5G0TGFVTtYs3tBxiYNxv"
 consumer_secret = "4GbcPy3YQ6r09hhYVBDiEffPe8msSfPJpukI2W2t3D5W7jceMy"
 access_token = "930170666514599937-cYnnkdz88njKaxxAsF69eVkiPPZgo4R"
