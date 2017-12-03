525b7c869a8a38138205f951a9d6c1354d27e6ab
txstu09 <sgillies09@gmail.com>
2017-12-02 19:44:24 -0600

    removed config from public repo

--- config.py
+++
@@ -1,6 +0,0 @@
-#Twitter API keys for @txstu09
-
-consumer_key = "avKFx5G0TGFVTtYs3tBxiYNxv"
-consumer_secret = "4GbcPy3YQ6r09hhYVBDiEffPe8msSfPJpukI2W2t3D5W7jceMy"
-access_token = "930170666514599937-cYnnkdz88njKaxxAsF69eVkiPPZgo4R"
-access_token_secret = "MZ4D7h8ad7xCFwquXKTKqJvb5EbUbz2c0H5Y3WLz2ScvB"
